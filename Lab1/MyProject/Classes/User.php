558b7e53ab8dc5eca7ae146ac92360580db222a2
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-03-14 13:13:11 +0300

    Update User.php

--- Lab1/MyProject/Classes/User.php
+++ Lab1/MyProject/Classes/User.php
@@ -1,10 +1,7 @@
 <?php
 namespace MyProject\Classes;
-class User {
-    public $name;
-    public $login;
-    private $password;
-
+class User extends AbstractUser {
+    
     public function __construct($name, $login, $password) {
         $this->name = $name;
         $this->login = $login;

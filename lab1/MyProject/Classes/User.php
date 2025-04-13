deb93a63f105184f46369b0bc948f7ca9a056a96
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 17:27:22 +0300

    Create User.php

---
+++ lab1/MyProject/Classes/User.php
@@ -0,0 +1,20 @@
+
+//Первое задание
+<?php
+
+class User {
+    public $name;
+    public $login;
+    private $password;
+
+    public function __construct($name, $login, $password) {
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password;
+    }
+
+    public function showInfo() {
+        echo "Имя: $this->name, Логин: $this->login, Пароль: $this->password<br>";
+    }
+}
+?>

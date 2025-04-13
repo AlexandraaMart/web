8908ecc7babf5fd621f8e5c430e5bcdf4bc5ea48
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 19:27:43 +0300

    Update users.php

--- lab1/users.php
+++ lab1/users.php
@@ -65,3 +65,39 @@ $user = new SuperUser('Сергей', 'serg', '123456', 'Administrator');
 $user->showInfo();
 ?>
 
+
+
+
+//седьмое задание
+
+<?php
+declare(strict_types=1);
+
+use MyProject\Classes\User;
+use MyProject\Classes\SuperUser;
+
+spl_autoload_register(function ($class_name) {
+    $path = __DIR__.'/'. str_replace('\\', '/', $class_name) . '.php'; 
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+$user1 = new User('Алексей', 'alex', '123');
+$user2 = new User('Мария', 'maria', '1234');
+//$user3 = new User('Иван', 'ivan', '12345');
+
+$user1->showInfo();
+$user2->showInfo();
+//$user3->showInfo();
+
+//$user = new SuperUser('Сергей', 'serg', '123456', 'Administrator');
+//$user->showInfo();
+
+$superUser = new SuperUser("Иван", "ivan", "12345", "admin");
+
+$info = $superUser->getInfo();
+print_r($info)
+?>
+
+

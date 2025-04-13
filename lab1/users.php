bc373434456530d4b6cb806d095f7a19b6279ca7
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 17:44:53 +0300

    Update users.php

--- lab1/users.php
+++ lab1/users.php
@@ -12,3 +12,25 @@ $user1->showInfo();
 $user2->showInfo();
 $user3->showInfo();
 ?>
+
+
+
+
+//третье задание
+<?php
+
+require_once 'MyProject/Classes/User.php';
+require_once 'MyProject/Classes/SuperUser.php';
+
+$user1 = new User('Алексей', 'alex', '123');
+$user2 = new User('Мария', 'maria', '1234');
+$user3 = new User('Иван', 'ivan', '12345');
+
+$user1->showInfo();
+$user2->showInfo();
+$user3->showInfo();
+
+$user = new SuperUser('Сергей', 'serg', '123456', 'Administrator');
+
+$user->showInfo();
+?>

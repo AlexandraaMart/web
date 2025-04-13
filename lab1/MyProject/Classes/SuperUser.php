03b735fd83ba16cca6e61dd69e85d20dfc7eb445
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 17:42:07 +0300

    Create SuperUser.php

---
+++ lab1/MyProject/Classes/SuperUser.php
@@ -0,0 +1,19 @@
+//третье задание
+<?php
+
+   require_once 'User.php';
+
+   class SuperUser extends User {
+       public $role;
+
+       public function __construct($name, $login, $password, $role) {
+           parent::__construct($name, $login, $password);
+           $this->role = $role;
+       }
+
+       public function showInfo() {
+           echo "Имя: $this->name, Логин: $this->login, Роль: $this->role<br>";
+       }
+   }
+   ?>
+   

4ae5a0afea74fd951cdbf871b267d36e62e36659
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 19:38:53 +0300

    Update SuperUser.php

--- lab1/MyProject/Classes/SuperUser.php
+++ lab1/MyProject/Classes/SuperUser.php
@@ -51,5 +51,45 @@ class SuperUser extends User implements SuperUserInterface {
 }
 ?>
 
+//восьмое задание
+<?php
+declare(strict_types=1);
+namespace MyProject\Classes;
+
+require_once 'User.php';
+require_once 'SuperUserInterface.php';
+
+class SuperUser extends User implements SuperUserInterface {
+    public $role;
+    public $password;
+    private static $superUserCount = 0;
+
+    public function __construct($name, $login, $password, $role) {
+        parent::__construct($name, $login, $password);
+        $this->role = $role;
+        $this->password = $password;
+        self::$superUserCount++;
+    }
+
+    public function showInfo() {
+        echo "Имя: $this->name, Логин: $this->login, Роль: $this->role<br>";
+    }
+
+    public function getInfo(): array {
+        return [
+            'name' => $this->name,
+            'login' => $this->login,
+            'password' => $this->password,
+            'role' => $this->role
+        ];
+    }
+    
+    public static function getSuperUserCount() {
+        return self::$superUserCount;
+    }
+}
+?>
+
+
 
    

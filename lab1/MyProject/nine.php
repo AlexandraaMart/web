517f67d1a8db4ad0719fd28187c0e95bd38b9c32
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 20:03:03 +0300

    Create nine.php

---
+++ lab1/MyProject/nine.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Диаграмма классов</title>
+    <style>
+        img {
+            max-width: 100%; /* Уменьшает изображение до ширины контейнера */
+            height: auto; /* Соотношение сторон сохраняется */
+        }
+    </style>
+</head>
+<body>
+    <h1>Диаграмма классов</h1>
+    <img src="Диаграмма.JPG" alt="Диаграмма классов">
+</body>
+</html>
+

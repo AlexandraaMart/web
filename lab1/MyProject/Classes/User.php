bdee016419d7d5daae6c5d805ba5d1ca6473f99c
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-13 19:39:22 +0300

    Update User.php

--- lab1/MyProject/Classes/User.php
+++ lab1/MyProject/Classes/User.php
@@ -25,3 +25,38 @@ class User {
        }
 }
 ?>
+
+//восьмое задание
+<?php
+declare(strict_types=1);
+namespace MyProject\Classes;
+
+require_once 'AbstractUser.php';
+
+class User {
+    public $name;
+    public $login;
+    private $password;
+    private static $userCount = 0;
+
+    public function __construct($name, $login, $password) {
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password;
+        self::$userCount++;
+    }
+
+    public function showInfo() {
+        echo "Имя: $this->name, Логин: $this->login, Пароль: $this->password<br>";
+    }
+    
+    public function __destruct() {
+           echo "Пользователь $this->login удален.";
+       }
+       
+    public static function getUserCount() {
+        return self::$userCount;
+    }
+}
+?>
+

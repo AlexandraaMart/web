5444998f6f26f02e8b9ea1132d6730c50b7d3f03
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-23 22:34:48 +0300

    Update AbstractUser.php

--- lab1/MyProject/Classes/AbstractUser.php
+++ lab1/MyProject/Classes/AbstractUser.php
@@ -1,24 +1,8 @@
 <?php
 declare(strict_types=1);
 namespace MyProject\Classes;
-
 abstract class AbstractUser {
-    protected $name;
-    protected $login;
-    protected $password;
-
-    public function __construct($name, $login, $password) {
-        $this->name = $name;
-        $this->login = $login;
-        $this->password = $password;
-    }
 
     abstract public function showInfo();
-    
-    public function __destruct() {
-        echo "Пользователь $this->login удален.";
-    }
 }
 ?>
-
-

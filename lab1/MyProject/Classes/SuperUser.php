58b7f49b30ec517d7fd5ce1cc43562566dfadd7a
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-23 22:05:31 +0300

    Update SuperUser.php

--- lab1/MyProject/Classes/SuperUser.php
+++ lab1/MyProject/Classes/SuperUser.php
@@ -1,95 +1,32 @@
-//третье, четвертое задание
-<?php
-   namespace MyProject\Classes;
-
-   require_once 'User.php';
-
-   class SuperUser extends User {
-       public $role;
-
-       public function __construct($name, $login, $password, $role) {
-           parent::__construct($name, $login, $password);
-           $this->role = $role;
-       }
-
-       public function showInfo() {
-           echo "Имя: $this->name, Логин: $this->login, Роль: $this->role<br>";
-       }
-   }
-   ?>
-
-//седьмое задание
 <?php
 declare(strict_types=1);
 namespace MyProject\Classes;
 
-require_once 'User.php';
-require_once 'SuperUserInterface.php';
+class SuperUser extends User implements SuperUserInterface{
 
-class SuperUser extends User implements SuperUserInterface {
-    public $role;
-    public $password;
-
-    public function __construct($name, $login, $password, $role) {
-        parent::__construct($name, $login, $password);
-        $this->role = $role;
-        $this->password = $password;
-    }
-
-    public function showInfo() {
-        echo "Имя: $this->name, Логин: $this->login, Роль: $this->role<br>";
-    }
-
-    public function getInfo(): array {
-        return [
-            'name' => $this->name,
-            'login' => $this->login,
-            'password' => $this->password,
-            'role' => $this->role
-        ];
-    }
-}
-?>
-
-//восьмое задание
-<?php
-declare(strict_types=1);
-namespace MyProject\Classes;
-
-require_once 'User.php';
-require_once 'SuperUserInterface.php';
-
-class SuperUser extends User implements SuperUserInterface {
-    public $role;
-    public $password;
-    private static $superUserCount = 0;
-
-    public function __construct($name, $login, $password, $role) {
-        parent::__construct($name, $login, $password);
-        $this->role = $role;
-        $this->password = $password;
-        self::$superUserCount++;
-    }
-
-    public function showInfo() {
-        echo "Имя: $this->name, Логин: $this->login, Роль: $this->role<br>";
-    }
-
-    public function getInfo(): array {
-        return [
-            'name' => $this->name,
-            'login' => $this->login,
-            'password' => $this->password,
-            'role' => $this->role
-        ];
-    }
-    
-    public static function getSuperUserCount() {
-        return self::$superUserCount;
-    }
-}
-?>
-
-
-
+   public $role;
+   public static $superUserCount = 0;
    
+	
+   function __construct($name, $login, $password, $role) {
+      $this->name = $name;
+      $this->login = $login;
+      $this->password = $password;
+      $this->role = $role;
+      self::$superUserCount++;
+   }
+   
+	       
+   function showInfo(){
+        echo "<p><b>name:</b> {$this->name} <b>login:</b> {$this->login} <b>password:</b> {$this->password} <b>role:</b> {$this->role} </p>";
+   }  
+   
+   function getInfo()
+   {
+       return print_r(['name' => $this->name,
+               'login' => $this->login,
+               'password' => $this->password,
+               'role' => $this->role ]);
+
+   }
+}

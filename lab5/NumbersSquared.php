2d0571df70d42092476b21928bb9a750a362d135
AlexandraaMart <126903794+AlexandraaMart@users.noreply.github.com>
2025-04-29 22:12:27 +0300

    Add files via upload

--- lab5/NumbersSquared.php
+++ lab5/NumbersSquared.php
@@ -1,42 +1,45 @@
 <?php
 
-class NumbersSquared implements Iterator {
-    private $start;   
-    private $end;     
-    private $current; 
+class NumbersSquared implements Iterator{
+    private $start;
+    private $end;
+    private $current;
 
-    public function __construct(int $start, int $end) {
+    public function __construct(int $start, int $end)
+    {
         $this->start = $start;
         $this->end = $end;
-        $this->current = $start; //текущее значение
+        $this->rewind();
+    }
+    public function rewind(): void
+    {
+        $this->current = $this->start;
     }
 
-    public function rewind(): void {
-        $this->current = $this->start; 
+    public function valid(): bool
+    {
+        return $this->current <= $this->end;
     }
 
-    public function valid(): bool {
-        return $this->current <= $this->end; 
+    public function next(): void
+    {
+        $this->current++;
     }
 
-    public function next(): void {
-        $this->current++; 
+    public function key(): int
+    {
+        return $this->current;
     }
 
-    public function key(): mixed {
-        return $this->current; 
-    }
-
-    public function current(): mixed {
-        return $this->current * $this->current; 
+    public function current(): int
+    {
+        return $this->current ** 2;
     }
 }
 
+// Создаем объект класса NumbersSquared от 3 до 7
 $obj = new NumbersSquared(3, 7);
 
 foreach ($obj as $num => $square) {
-    echo '<pre>';
     echo "Квадрат числа $num = $square\n";
-    echo '</pre>';
 }
-?>
